55be19ce798d2ae7e9dc6907c269022e41f099b0
Armin Novak <armin.novak@thincast.com>
2021-08-24 13:31:22 +0200

    xxx

--- libfreerdp/core/server.c
+++ libfreerdp/core/server.c
@@ -1313,8 +1313,6 @@ BOOL WINAPI FreeRDP_WTSVirtualChannelClose(HANDLE hChannelHandle)
 	rdpPeerChannel* channel = (rdpPeerChannel*)hChannelHandle;
 	BOOL ret = TRUE;
 
-	WINPR_ASSERT(channel);
-
 	if (channel)
 	{
 		WTSVirtualChannelManager* vcm = channel->vcm;
